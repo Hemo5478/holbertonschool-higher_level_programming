88458dc24f8f807977ddb7ea93a68f270431c2c4
Hemo5478 <hamza9638@hotmail.com>
2023-07-21 13:13:31 +0100

    Fixing

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-	char = chr(i)
-	if char not in ['q', 'e']:
-		print("{:s}".format(char), end='')
+    if i in [101, 113]:
+        continue
+    print("{}".format(chr(i)), end='')
