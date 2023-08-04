0df4abd789019ae1e1bea6221f9f8ca348767ae9
Hemo5478 <hamza9638@hotmail.com>
2023-08-04 22:35:26 +0100

    Fixing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-a = str(number)
-length = len(a)
-b = a[length-1:length]
-last_digit = int(b)
+last_digit = abs(number) % 10
 if last_digit > 5:
     message = 'and is greater than 5'
 elif last_digit < 6 and last_digit != 0:
