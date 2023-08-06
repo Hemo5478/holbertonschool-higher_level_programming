9bad5524f954048a2efd6a0ef61ab3e9c489ee07
Hemo5478 <hamza9638@hotmail.com>
2023-08-07 00:27:09 +0100

    Fixing

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,5 +2,7 @@
 from sys import argv
 if __name__ == '__main__':
     args = argv[1:]
-    add_arg = sum(map(int, args))
+    add_arg = 0
+    for arg in args:
+        add_arg += int(arg)
     print(add_arg)
