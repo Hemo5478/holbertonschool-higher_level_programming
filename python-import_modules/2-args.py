198f7ce14010f5ca97a434a4f04aa2abdefbdd80
Hemo5478 <hamza9638@hotmail.com>
2023-08-07 00:13:23 +0100

    Fixing

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == '__main__':
-    num = len(argv) - 1
-    if num == 0:
-        print("No arguments.")
-    elif num == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(num))
-    for i in range(1, num + 1):
-        arg = argv[i]
-        print("{:d}: {}".format(i, arg))
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    print("{:d} argument{}".format(num_args, "" if num_args == 1 else "s"), end="")
+    print("{}".format("." if num_args == 0 else ":"))
+    if num_args > 0:
+        for i in range(1, num_args + 1):
+            print("{:d}: {}".format(i, argv[i]))
