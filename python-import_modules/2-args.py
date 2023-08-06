414066b482e83ad9cea3946561163721d635fb41
Hemo5478 <hamza9638@hotmail.com>
2023-08-07 00:01:14 +0100

    Args ugh

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+    num = len(argv) - 1
+    if num == 0:
+        print("No arguments.")
+    elif num == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num))
+    for i in range(1, num + 1):
+        arg = argv[i]
+        print("{}: {}".format(i, arg))
