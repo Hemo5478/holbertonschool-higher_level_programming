411eca7edf0483dbde1c4735f0b6a06afbe48ce5
Hemo5478 <hamza9638@hotmail.com>
2023-08-09 00:31:19 +0100

    Fixed

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_values = []
-    i = 0
-    while i < len(matrix):
-        matrix_row = matrix[i]
-        list_values = []
-        for columns in range(len(matrix_row)):
-            list_values.append(matrix_row[columns] ** 2)
-            matrix_values.append(list_values)
-            i += 1
-            return matrix_values
+    new = []
+    for row in matrix:
+        compt = []
+        for i in row:
+            compt.append(i ** 2)
+        new.append(compt)
+    return new
