96d589a8e98119dc1fcf574b75354618143106fe
Hemo5478 <hamza9638@hotmail.com>
2023-08-09 00:21:33 +0100

    Fixing

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_values = []
+    i = 0
+    while i < len(matrix):
+        matrix_row = matrix[i] 
+        list_values = []
+        for columns in range(len(matrix_row)):
+            list_values.append(matrix_row[columns] ** 2)
+        matrix_values.append(list_values)
+        i += 1
+    return matrix_values
