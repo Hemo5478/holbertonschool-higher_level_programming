a7d5c55babd85530de1cdc09e8d0ef8fbde35985
Hemo5478 <hamza9638@hotmail.com>
2023-08-09 20:59:55 +0100

    Fixing ugh

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == []:
+        return (0)
+    tot_type_1 = 0
+    tot_type_2 = 0
+    for score, weight in my_list:
+        tot_type_1+= score * weight
+        tot_type_2+= weight
+    avg = tot_type_1 / tot_type_2
+    return (avg)
