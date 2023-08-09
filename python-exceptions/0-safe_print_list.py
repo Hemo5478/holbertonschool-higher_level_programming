c1c65f9fbd3aa1f31826248d729d051a042d5716
Hemo5478 <hamza9638@hotmail.com>
2023-08-09 21:15:10 +0100

    Yee while next time

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    i = 0
-    while i < x:
+    compt = 0
+    for i in range(0, x):
         try:
-            print(my_list[i], end=" ")
-            count += 1
-            i += 1
+            print(f"{my_list[i]}", end="")
+            compt += 1
         except IndexError:
-            break
+            pass
     print()
-    return (count)
+    return (compt)
