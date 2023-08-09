31e2311d30d3d080670ce3bbe2dd00721fdeff25
Hemo5478 <hamza9638@hotmail.com>
2023-08-09 21:01:51 +0100

    Fixed

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
     tot_type_1 = 0
     tot_type_2 = 0
     for score, weight in my_list:
-        tot_type_1+= score * weight
-        tot_type_2+= weight
+        tot_type_1 += score * weight
+        tot_type_2 += weight
     avg = tot_type_1 / tot_type_2
     return (avg)
