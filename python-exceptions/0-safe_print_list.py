4f3f70ea2e0690d9aec74c470d80277a6c3cd01c
Hemo5478 <hamza9638@hotmail.com>
2023-08-09 21:10:32 +0100

    Working ugh

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        count = 0
+        try:
+            print(my_list[i], end="")
+            compt += 1
+        except IndexError:
+            break
+    print()
+    return (compt)
