24142a1538411753b1729a64b38c4fe82d2f83b2
Hemo5478 <hamza9638@hotmail.com>
2023-08-11 16:43:54 +0100

    Fixing

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(10):
+    for y in range(i + 1, 10):
+        if i != y:
+            if i == 8 and y == 9:
+                print("{}{}".format(i, y))
+            else:
+                print("{:d}{:d}, ".format(i, y), end="")
